5b813c665f6673e2cab4be40808bd8c43edfb05d
Louis Fernand <louisfernand63@gmail.com>
2023-11-12 19:15:51 +0100

    Esercizio 10_11_2023 JS

--- Esercizio 10_11_2023 JS/assets/js/script.js
+++ Esercizio 10_11_2023 JS/assets/js/script.js
@@ -1,52 +1,69 @@
 
 let tombola = () => {
-    let table = document.querySelector("div table");
+    let table = document.querySelector(".main-board");
     for (let i = 0; i < 76; i++) {
-        let table_element = document.createElement("div");
-
-        table_element.className = "table_element";
-        table_element.innerText = [i + 1];
-
-        table_element.style.width = "10%";
-        table_element.style.height = "10%";
-        table_element.style.backgroundColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-        table_element.style.border = "1px solid #000";
-        table_element.style.display = "inline-block";
-        table_element.style.margin = "1rem";
-        table_element.style.textAlign = "center";
-        table_element.style.verticalAlign = "middle";
-        table_element.style.lineHeight = "10rem";
-        table_element.style.fontSize = "4rem";
-        table_element.style.fontWeight = "bold";
-        table_element.style.color = "#fff";
-        table_element.style.borderRadius = "50%";
-        table_element.style.boxShadow = "0 0 10px #000";
-        table_element.style.cursor = "pointer";
-        table_element.style.padding = "1rem";
-
-
-        table.appendChild(table_element);
+        table.innerHTML += `<div class='cell'>${i + 1}</div>`;
     }
 }
 tombola();
 
-let faiLaTuaGiocata = () => {
-    let btn = document.querySelector(".btn");
-    btn.style.cursor = "pointer";
-    btn.addEventListener("click", () => {
-        let primaEstrazione = Math.floor(Math.random() * 76);
-        //console.log(primaEstrazione);
-        for (let i = 0; i < 76; i++) {
-            let table_element_number = [i + 1];
-            /*let table_element = document.createElement("div");*/
+let fillArray = function () {
+    let arr = []
+    for (let i = 0; i < 76; i++) {
+        arr.push(i + 1);
+    }
+    return arr
+}
+fillArray();
 
-            table_element.className = "table_element";
-            if (primaEstrazione === table_element_number) {
-                table_element.style.backgroundColor = "green";
-            } else {
-                table_element.style.backgroundColor = "red";
-            }
+let getRandomNumber = () => {
+    let randIndex = Math.floor(Math.random() * range.length);
+    let random = range.length.splice(randIndex, 1)[0];
+    return random;
+}
+getRandomNumber();
+
+let generateRandNumber = (range) => {
+    let randNumber = getRandomNumber(range);
+    let randNumDiv = document.querySelector('randNum');
+    randNumDiv.innerText = 'Numero: ' + randNumber;
+
+    let cells = document.querySelectorAll('.main-board .cell');
+    cells[random - 1].classList.add('highlight');
+
+    let userCells = document.querySelectorAll('.user-board .cell')
+    userCells.forEach((cell) => {
+        if (parseInt(cell.innerText) === random) {
+            cell.classList.add('highlight')
         }
     })
 }
-faiLaTuaGiocata();
+generateRandNumber();
+
+let generateUserBoards = () => {
+    let usersNumber = document.querySelector('#usersNumber').value
+    let container = document.querySelector('.container')
+    if (parseInt(usersNumber) > 0) {
+        for (let i = 0; i < parseInt(usersNumber); i++) {
+            let range = fillArray()
+            let board = document.createElement('div')
+            board.className = 'board user-board'
+            for (let i = 0; i < 24; i++) {
+                let random = getRandomNum(range)
+                board.innerHTML += `<div class='cell'>${random}</div>`
+            }
+            container.appendChild(board)
+        }
+    }
+}
+
+window.onload = function () {
+    tombola();
+    let randBtn = document.querySelector('#randBtn');
+    let range = fillArray();
+    randBtn.addEventListener('click', function () {
+      generateRandNumber(range);
+    })
+    let userBoardBtn = document.querySelector('#userBoardBtn')
+    userBoardBtn.onclick = generateUserBoards;
+  }
